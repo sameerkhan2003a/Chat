fa996cc5250666ba553bd94d203fd7177323ea2d
sameerkhan2003a <153911733+sameerkhan2003a@users.noreply.github.com>
2023-12-17 23:31:18 +0530

    Update main.py

--- Chat/main.py
+++ Chat/main.py
@@ -10,31 +10,7 @@ app = Flask(__name__)
 
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
 
-
-    result = None 
-    if request.method == 'POST':
-        essayLength = request.form['essayLength']
-        essayQuality = request.form['essayQuality']
-        essayTopic = request.form['essayTopic']
-
-        client = openai.OpenAI(api_key=os.environ.get('secret_key'))
-     
-        response = client.chat.completions.create(
-            model="gpt-3.5-turbo-1106",
-            temperature=0,
-            max_tokens=1000,
-            messages=[
-                    {"role": "system", "content": f"You are a Writing Wizard who writes essays precisely of {essayLength} size or length and of {essayQuality} quality focused totally on the topic, finally be friendly with the user and do your job as perfectly as possible, Seperate the essay completely from your wizard intro, Be unbiased."},
-               { "role": "user", "content":  essayTopic},
-            ]
-        )
-        result = response.choices[0].message.content
-        
-
-    return render_template('index.html', result=result)
 
 
 
